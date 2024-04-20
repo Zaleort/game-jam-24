7fd05de661e366baeb71ad88b9c39e9e6fdc8df8
Alexandre Henrique Alves <zaleortgames@outlook.com>
2024-04-20 14:12:03 +0200

    Player oxygen sounds

--- Assets/Src/Player/Player.cs
+++ Assets/Src/Player/Player.cs
@@ -1,17 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class Player : MonoBehaviour
 {
+    public static int START_OXYGEN_LEVEL = 0;
+    public static int HIGH_OXYGEN_LEVEL = 1;
+    public static int HALF_OXYGEN_LEVEL = 2;
+    public static int LOW_OXYGEN_LEVEL = 3;
+
     public float oxygen = 100f;
     public float oxygenLossPerSecond = 1.0f;
     public bool oxygenLossActivated = false;
 
-    // Start is called before the first frame update
-    void Start()
+    [FMODUnity.EventRef]
+    public string oxygenRefillEvent;
+
+    [FMODUnity.EventRef]
+    public string deathEvent;
+
+    [FMODUnity.EventRef]
+    public string breathingEvent;
+    private FMOD.Studio.EventInstance breathingInstance;
+
+    [FMODUnity.EventRef]
+    public string rapidBreathingEvent;
+    private FMOD.Studio.EventInstance rapidBreathingInstance;
+
+    public int actualOxygenLevelWarning = START_OXYGEN_LEVEL;
+
+    [FMODUnity.EventRef]
+    public string highOxygenWarning;
+
+    [FMODUnity.EventRef]
+    public string halfOxygenWarning;
+
+    [FMODUnity.EventRef]
+    public string lowOxygenWarning;
+
+  // Start is called before the first frame update
+  void Start()
     {
-        
+      breathingInstance = RuntimeManager.CreateInstance(breathingEvent);
+      rapidBreathingInstance = RuntimeManager.CreateInstance(rapidBreathingEvent);
     }
 
     // Update is called once per frame
@@ -20,7 +52,13 @@ public class Player : MonoBehaviour
         ConsumeOxygen();
     }
 
-    public void ActivateOxygenConsumption()
+    private void OnDestroy()
+    {
+      breathingInstance.release();
+      rapidBreathingInstance.release();
+    }
+
+  public void ActivateOxygenConsumption()
     {
       oxygenLossActivated = true;
     }
@@ -33,6 +71,8 @@ public class Player : MonoBehaviour
       }
 
       oxygen -= oxygenLossPerSecond * Time.deltaTime;
+      ManageBreathingEvents();
+      PlayOxygenWarning();
 
       CheckIfDead();
     }
@@ -44,13 +84,48 @@ public class Player : MonoBehaviour
       {
         oxygen = 100;
       }
+
+      RuntimeManager.PlayOneShot(oxygenRefillEvent, transform.position);
     }
 
     private void CheckIfDead()
     {
       if (oxygen <= 0)
       {
+        RuntimeManager.PlayOneShot(deathEvent, transform.position);
         GameController.Instance.GameOver();
       } 
+    }
+
+    private void ManageBreathingEvents()
+    {
+      if (oxygen >= 20 && !breathingInstance.isValid()) {
+        rapidBreathingInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        breathingInstance.start();
+      } else if (oxygen < 20 && !rapidBreathingInstance.isValid()) {
+        breathingInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        rapidBreathingInstance.start();
+      }
+    }
+
+    private void PlayOxygenWarning()
+    {
+      if (oxygen <= 80 && oxygen > 50 && actualOxygenLevelWarning != HIGH_OXYGEN_LEVEL)
+      {
+        actualOxygenLevelWarning = HIGH_OXYGEN_LEVEL;
+        RuntimeManager.PlayOneShot(highOxygenWarning, transform.position);
+      }
+
+      if (oxygen <= 50 && oxygen > 20 && actualOxygenLevelWarning != HALF_OXYGEN_LEVEL)
+      {
+        actualOxygenLevelWarning = HALF_OXYGEN_LEVEL;
+        RuntimeManager.PlayOneShot(halfOxygenWarning, transform.position);
+      }
+
+      if (oxygen <= 20 && actualOxygenLevelWarning != LOW_OXYGEN_LEVEL)
+      {
+        actualOxygenLevelWarning = LOW_OXYGEN_LEVEL;
+        RuntimeManager.PlayOneShot(lowOxygenWarning, transform.position);
+      }
     }
 }
